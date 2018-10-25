b083ad3fa78af1bfdaeae89aacb03136bae62834
John Myrda <johnmyrda@gmail.com>
2018-10-25 15:40:53 -0700

    experimenting with named pipes

--- src/led_sim.c
+++ src/led_sim.c
@@ -24,6 +24,12 @@
 #include <libgen.h>
 #include <string.h>
 #include <pthread.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "sim_avr.h"
 #include "avr_ioport.h"
@@ -40,12 +46,7 @@ pthread_mutex_t avr_mutex;
 avr_t * avr = NULL;
 uint8_t	pin_state = 0;	// current port B
 
-float pixsize = 64;
-int window;
-
-void ws2811_changed_hook(struct avr_irq_t * irq, uint32_t value, void * param){
-	printf("Value: %d\n", value);
-}
+FILE * fp;//file descriptor for named pipe
 
 void keyCB(unsigned char key, int x, int y)	/* called on key press */
 {
@@ -84,7 +85,7 @@ static void * avr_run_thread(void * oaram)
 //debug function to print truecolor rgb in terminal
 void pixels_to_truecolor(const rgb_pixel_t * pixels, uint32_t strip_length){
         const char filled_square_format_string[] = "\x1b[47m\x1b[38;2;%d;%d;%dm\u25A0 \x1b[0m";
-        const char empty_square_string[]         = "\x1b[47m\x1b[38;2;255;255;255m\u25A1 \x1b[0m";
+        const char empty_square_string[]         = "\x1b[47m\x1b[38;2;0;0;0m\u25A1 \x1b[0m";
 
         printf("strip_length: %d, hex: ", strip_length);
         for(int i=0; i < strip_length; i++){
@@ -107,6 +108,12 @@ void pixels_to_truecolor(const rgb_pixel_t * pixels, uint32_t strip_length){
 void pixels_done_hook(const rgb_pixel_t * pixels, const uint32_t strip_length, const uint64_t time_in_ns){
         printf("time: %ldns\n", time_in_ns);
         pixels_to_truecolor(pixels, strip_length);
+
+        int write_result = fwrite(pixels, sizeof(rgb_pixel_t), strip_length, fp);
+        if(write_result < -1){
+                printf("write error!");
+                }
+        fflush(fp);
 }
 
 int main(int argc, char *argv[])
@@ -159,7 +166,21 @@ int main(int argc, char *argv[])
 	// 'raise' it, it's a "pullup"
 	avr_raise_irq(button.irq + IRQ_BUTTON_OUT, 1);
 
-	// the AVR run on it's own thread. it even allows for debugging!
+	// create a named pipe
+
+        char pipe_name[] = "/tmp/neopixel";
+        int remove_error = remove(pipe_name);
+        if(remove_error < 0){
+                printf("can't remove %s ", pipe_name);
+                perror("error:");
+                }
+        int mkfifo_error = mkfifo(pipe_name, 0666);
+        if(mkfifo_error < 0){
+                perror("mkfifo failed");
+                }
+        fp = fopen(pipe_name, "w");
+        
+        // the AVR run on it's own thread. it even allows for debugging!
 
 	pthread_t run;
 	pthread_create(&run, NULL, avr_run_thread, NULL);
@@ -169,8 +190,9 @@ int main(int argc, char *argv[])
 		pthread_mutex_lock(&avr_mutex);
 		time_nsec = avr_cycles_to_nsec(avr, avr->cycle);
 		pthread_mutex_unlock(&avr_mutex);
-		//printf("a ");
 	}
+
+        fclose(fp);
 
 	printf("time stopped at %ldns\n", time_nsec);
 	exit(0);
