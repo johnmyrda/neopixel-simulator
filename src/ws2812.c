e8ef3c31e086ef2dd2129f874f3d590aeaec2572
John Myrda <hello@johnmyrda.com>
2019-09-30 20:24:38 -0700

    remove avr references and switch tabs to spaces where possible

--- src/ws2812.c
+++ src/ws2812.c
@@ -8,23 +8,13 @@ const timing_t T1H = {550, 5500};
 const timing_t TLD = {300, 5000};
 const timing_t TLL = {6000, UINT16_MAX};
 
-// void ws2812_pin_changed_hook(struct avr_irq_t * irq, uint32_t value, void *param){
-//     ws2812_t *led_metadata = (ws2812_t *) param;
-// //    uint64_t time_nsec = avr_cycles_to_nsec(led_metadata->avr, led_metadata->avr->cycle);
-// //    printf("Pin state: %d\n", value);
-//     ws2812_run(time_nsec, value, led_metadata);
-// }
-
 void ws2812_init(ws2812_t * led_metadata, rgb_pixel_t * pixels, uint32_t strip_length, latch_callback_t cb){
     *led_metadata = (struct ws2812_t) {0};
 
-    //led_metadata->avr = avr;
-    // led_metadata->irq = avr_alloc_irq(&avr->irq_pool, 0, 1, NULL);
     memset(pixels, 0, sizeof(rgb_pixel_t)*strip_length);
     led_metadata->pixels = pixels;
     led_metadata->strip_length = strip_length;
     led_metadata->latch_callback = cb;
-    //avr_irq_register_notify(led_metadata->irq, ws2812_pin_changed_hook, led_metadata);
 }
 
 _Bool ws2812_high(uint64_t time){
@@ -39,31 +29,31 @@ _Bool ws2812_high(uint64_t time){
 }
 
 void ws2812_low(uint64_t time, ws2812_t * led_metadata){
-	if(TLD.low < time && time < TLD.high){
-                //set bit using bitmask
-		led_metadata->cur_byte = (led_metadata->cur_bit << (7 - led_metadata->cur_bit_index)) | led_metadata->cur_byte;
-		led_metadata->cur_bit_index++;
-		//printf(" set bit %d\n", led_metadata->cur_bit);
-		if(led_metadata->cur_bit_index >= 8){
-			led_metadata->cur_bit_index = 0;
-			//printf("byte value: %d\n", led_metadata->cur_byte);
+    if(TLD.low < time && time < TLD.high){
+        //set bit using bitmask
+        led_metadata->cur_byte = (led_metadata->cur_bit << (7 - led_metadata->cur_bit_index)) | led_metadata->cur_byte;
+        led_metadata->cur_bit_index++;
+        //printf(" set bit %d\n", led_metadata->cur_bit);
+        if(led_metadata->cur_bit_index >= 8){
+            led_metadata->cur_bit_index = 0;
+            //printf("byte value: %d\n", led_metadata->cur_byte);
                         if(led_metadata->cur_byte_index < led_metadata->strip_length*sizeof(rgb_pixel_t)){
                                 //cast the pixels to a uint8_t pointer so i can count by bytes
                                 ((uint8_t *)led_metadata->pixels)[led_metadata->cur_byte_index] = led_metadata->cur_byte;
                                 //printf("byte index: %d pixel size %ld\n", led_metadata->cur_byte_index, sizeof(rgb_pixel_t));
                                 }
-			led_metadata->cur_byte = 0;
-			led_metadata->cur_byte_index++;
-		}
-	} else
-	if(TLL.low < time && led_metadata->cur_byte_index){
-		led_metadata->cur_byte = (led_metadata->cur_bit << led_metadata->cur_bit_index) | led_metadata->cur_byte;
-		//printf("latched.");
+            led_metadata->cur_byte = 0;
+            led_metadata->cur_byte_index++;
+        }
+    } else
+    if(TLL.low < time && led_metadata->cur_byte_index){
+        led_metadata->cur_byte = (led_metadata->cur_bit << led_metadata->cur_bit_index) | led_metadata->cur_byte;
+        //printf("latched.");
         led_metadata->latch_callback(led_metadata->pixels, led_metadata->cur_byte_index / sizeof(rgb_pixel_t), led_metadata->last_pin_change_time);
-		led_metadata->cur_byte = 0;
-		led_metadata->cur_bit_index = 0;
-		led_metadata->cur_byte_index = 0;
-	}
+        led_metadata->cur_byte = 0;
+        led_metadata->cur_bit_index = 0;
+        led_metadata->cur_byte_index = 0;
+    }
 }
 
 
--- src/ws2812.h
+++ src/ws2812.h
@@ -7,34 +7,30 @@
 #include "sim_time.h"
 
 typedef struct rgb_pixel_t {
-	uint8_t red;
-	uint8_t green;
-	uint8_t blue;
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
 } rgb_pixel_t;
 
 typedef void (* latch_callback_t)(const rgb_pixel_t * pixels, const uint32_t strip_length, const uint64_t time_in_ns);
 
 typedef struct ws2812_t
 {
-	// avr_irq_t * irq;
-	// struct avr_t * avr;
     uint64_t last_pin_change_time;
     latch_callback_t latch_callback;
     rgb_pixel_t * pixels;
     uint32_t strip_length;
     uint8_t cur_bit;
-	uint8_t cur_bit_index;
-	uint8_t cur_byte;
-	uint8_t cur_byte_index;
+    uint8_t cur_bit_index;
+    uint8_t cur_byte;
+    uint8_t cur_byte_index;
 } ws2812_t;
 
 typedef struct timing_t
 {
-	uint16_t low;
-	uint16_t high;
+    uint16_t low;
+    uint16_t high;
 } timing_t;
-
-//void ws2812_pin_changed_hook(struct avr_irq_t * irq, uint32_t value, void *param);
 
 void ws2812_init(ws2812_t * led_metadata, rgb_pixel_t * pixels, uint32_t strip_length, latch_callback_t cb);
 
